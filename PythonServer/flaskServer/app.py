a1f0c19a832c8bd0211259f729b73168db6208bf
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-05-09 22:12:43 +0800

    template

--- PythonServer/flaskServer/app.py
+++ PythonServer/flaskServer/app.py
@@ -1,63 +1,25 @@
 from flask import Flask
-from flask import request
+from flask import request,render_template
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html')
 
 @app.route('/signin',methods=['GET'])
 def signin_form():
-    return '''<form action="/signin" method ="POST">
-                <p><input name="username"></p>
-                <p><input name="password"></p>
-                <p><button type="登陆">Sign In</button></p>
-                </form>
-                <html>
-    <head>
-        <title>
-            HELLO
-        </title>
-        
-        <style>
-     
-            h1{
-                color:#333333;
-                font-size: 48;
-                text-shadow: 3px 3px 3px #666666;
-                
-            }
-        </style>
-        <script>
-            function change(){
-                var a=true;
-                if(a)
-                    {
-                        document.getElementsByTagName('h1')[0].style.color='#ff0000';
-                        a=false;
-
-                    }
-                    
-                else
-                    document.getElementsByTagName('h1')[0].style.color='#666666';
-            }
-        </script>
-    </head>    
-    <body>
-      
-       <h1 onclick="change()">hello world</h1>
-    </body>
-    
-</html>
-                '''
+    return render_template('form.html')
 
 @app.route('/signin',methods=['POST'])
 def signin():
     # 需要从request对象读取表单内容
-    if request.form['username']=='admin' and request.form['password']=='password':
-        return '<h3>HELLO ,admin</h3>'
-    return '<h3>Bad userName or Password.</h3>'
+    username=request.form(['username'])
+    password=request.form(['password'])
+
+    if username=='admin' and password=='password':
+        return render_template('sign-ok.html',username=username)
+    return render_template('form.html',message='账号密码错误',username=username)
 
 if __name__=='__main__':
     app.run()
