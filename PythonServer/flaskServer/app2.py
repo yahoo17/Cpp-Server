2e17e04e4e54a2f1c0004c09630b58ebfff58ec2
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-05-28 23:04:46 +0800

    1

--- PythonServer/flaskServer/app2.py
+++ PythonServer/flaskServer/app2.py
@@ -7,12 +7,40 @@ app = Flask(__name__)
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
-    if request.method=='GET':
-        if request.
-        return 'you send server a GET request'
+    
+    if request.method=='GET':      
+            return 'you send server a GET request'
     elif request.method=='POST':
-        return 'you send server a POST request'
-    return "hello world"
+        return "you send server a POST request"
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    
+    # if request.method=='GET':      
+    #         return 'you send server a GET request'
+    # else request.method=='POST':
+    #     return "hello world"
+    if request.method=='GET':
+        name=request.args.get('name','')
+        if name=='yanhao':
+            return 'you send server a  request for yanhao'
+        elif name=='haoyan':
+            return 'you send server a  request for haoyan'
+        else:
+            return 'you send server a request for other user'
+    elif request.method=='POST':
+        # 如果要在POST body里面发消息
+        ## 需要在POST body里面
+        print(request.form)
+
+        name=request.json.get('name')
+        if name=='yanhao':
+            return dict(name='yanhao',fans=1000000,info='你给服务器我 发了一条POST,里面有 严灏 所以我给你返回')
+        elif  name=='haoyan':
+            return dict(name='haoyan',fans=10,info='你给服务器我 发了一条POST,里面有 灏严 所以我给你返回')
+        else :
+            return '你POST的BODY里 的RAW的JSON里面的name 不是我们已知的name'
+        
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
